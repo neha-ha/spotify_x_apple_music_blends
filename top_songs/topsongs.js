2a4a1179f1ccd1346471430b8261eb3830ead9be
kharnoor <39896520+kharnoor@users.noreply.github.com>
2024-05-02 13:25:29 -0700

    implemented blend using logic from previous meeting

--- top_songs/topsongs.js
+++ top_songs/topsongs.js
@@ -1,17 +1,33 @@
 const apiKey = '80036787eac16b24d14eba32990f4f82'; // Replace 'YOUR_API_KEY' with your actual Last.fm API key
-const username = 'kisslandxo'; 
+const username1 = 'kisslandxo'; 
+const username2 = 'sadhiika'; 
 const period = 'overall'; // You can change this to '7day', '1month', '3month', '6month', or '12month' as per your requirement
 
-const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username}&period=${period}&api_key=${apiKey}&format=json`;
+const apiUrl1 = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username1}&period=${period}&api_key=${apiKey}&format=json`;
+const apiUrl2 = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username2}&period=${period}&api_key=${apiKey}&format=json`;
 
-fetch(apiUrl)
+fetch(apiUrl1)
   .then(response => response.json())
   .then(data => {
     // Check if the response contains top tracks
     if (data && data.toptracks && data.toptracks.track) {
       // Loop through the tracks and format the output
       data.toptracks.track.forEach((track, index) => {
-        console.log(`"${track.name}" by "${track.artist.name}" (${track.playcount})`);
+        console.log(`${index + 1}."${track.name}" by "${track.artist.name}" (${track.playcount})`);
+      });
+    } else {
+      console.log('No top tracks found for the user.');
+    }
+  })
+  .catch(error => console.error('Error fetching data:', error));
+  fetch(apiUrl2)
+  .then(response => response.json())
+  .then(data => {
+    // Check if the response contains top tracks
+    if (data && data.toptracks && data.toptracks.track) {
+      // Loop through the tracks and format the output
+      data.toptracks.track.forEach((track, index) => {
+        console.log(`${index + 1}."${track.name}" by "${track.artist.name}" (${track.playcount})`);
       });
     } else {
       console.log('No top tracks found for the user.');
