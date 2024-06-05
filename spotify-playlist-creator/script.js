5aea088501321b1d8bf6c0703ff30c4684f1f748
neabbas <neabbas@ucsc.edu>
2024-06-04 17:52:36 -0700

    reduce repeats fixedish

--- spotify-playlist-creator/script.js
+++ spotify-playlist-creator/script.js
@@ -3,12 +3,9 @@ const period = 'overall'; // You can change this to '7day', '1month', '3month', 
 const limit = 500; // Number of top tracks to fetch for each user
 const blendLimit = 50; // Number of blended songs to include in the playlist
 
-genreScore = new Map();
-artistScore = new Map();
-songPriority = new Map();
-
-user1weight = 25;
-user2weight = 25;
+const genreScore = new Map();
+const artistScore = new Map();
+const songPriority = new Map();
 
 // fetches top songs using lastfm api: gets song name, artist name, and genre
 const getUserTopTracks = async (username) => {
@@ -17,7 +14,7 @@ const getUserTopTracks = async (username) => {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        //console.log(data.toptracks.track);
+        console.log(data.toptracks.track);
         const tracksWithGenres = await Promise.all(data.toptracks.track.map(async (track) => {
             const genre = await getTrackTopTags(track.artist.name, track.name);
             return {
@@ -29,7 +26,7 @@ const getUserTopTracks = async (username) => {
         }));
         return tracksWithGenres;
     } catch (error) {
-        //console.error(`Error fetching top tracks for user ${username}:`, error);
+        console.error(`Error fetching top tracks for user ${username}:`, error);
         return [];
     }
 };
@@ -39,7 +36,7 @@ const getTrackTopTags = async (artist, track) => {
     try {
         const response = await fetch(apiUrl2);
         const data = await response.json();
-        //console.log(data.toptags);
+        console.log(data.toptags);
         return (data.toptags.tag ? data.toptags.tag.map(tag => tag.name) : []);
     } catch (error) {
         // console.error(`Error fetching track tags for track ${track}:`, error);
@@ -47,43 +44,31 @@ const getTrackTopTags = async (artist, track) => {
     }
 }
 
-const updateSongScores = (userSongs, username, first) => { 
+const updateSongScores = (userSongs, username) => { 
     userSongs.forEach(song => { // get the songs and score them with the artists and genres
         const songKey = `${song.name} - ${song.artist}`;
         const songIndex = userSongs.indexOf(song);
         if (songPriority.has(songKey)) {
-            //console.log("already in:", songPriority.get(songKey));
+            console.log("already in:", songPriority.get(songKey));
             songPriority.set(songKey, [songPriority.get(songKey)[0] + (10 * Math.log(limit - songIndex)) + 50, songPriority.get(songKey)[1]]);
             songPriority.get(songKey)[1].push(username);
-            //console.log("already in:", songPriority.get(songKey));
         } else {
             if (song.genre) {
                 const genresScore = song.genre.reduce((totalScore, genre) => totalScore + genreScore.get(genre), 0);
-                //console.log("genre:", Math.log(artistScore.get(song.artist)), genresScore/2, (10 * Math.log(limit - songIndex)))
-                weight = 0;
-                if(first)
-                {
-                    weight = user1weight;
-                    user1weight--;
-                }else
-                {
-                    weight = user2weight;
-                    user2weight--;
-                }
-                songPriority.set(songKey, [weight * (Math.log(artistScore.get(song.artist)) + genresScore/2 + (10 * Math.log(limit - songIndex)) + 1), [username]]);
+                console.log("genre:", Math.log(artistScore.get(song.artist)), genresScore/2, (10 * Math.log(limit - songIndex)))
+                songPriority.set(songKey, [Math.log(artistScore.get(song.artist)) + genresScore/2 + (10 * Math.log(limit - songIndex)) + 1, [username]]);
             }
             else {
-                //console.log("no genre:", Math.log(artistScore.get(song.artist)), (10 * Math.log(limit - songIndex)))
-                songPriority.set(songKey, [0.5 * Math.log(artistScore.get(song.artist)) + (10 * Math.log(limit - songIndex)) + 1, [username]]);
+                console.log("no genre:", Math.log(artistScore.get(song.artist)), (10 * Math.log(limit - songIndex)))
+                songPriority.set(songKey, [Math.log(artistScore.get(song.artist)) + (10 * Math.log(limit - songIndex)) + 1, [username]]);
             }
         }
     });
 };
 
-const updateArtistScores = (userSongs) => { // get the genres and score them
+const updateGenreScores = (userSongs) => { // get the genres and score them
     userSongs.forEach(song => {
         song.genre = song.genre.slice(0, 10);
-        //console.log("genres for", song.name, ":", song.genre);
         Array.prototype.forEach.call(song.genre, genre => {
             if (genreScore.has(genre)) {
                 genreScore.set(genre, genreScore.get(genre) + 0.1);
@@ -94,30 +79,59 @@ const updateArtistScores = (userSongs) => { // get the genres and score them
     });
 };
 
-const updateGenreScores = (userSongs) => { // get the artists and score them
+const updateArtistScores = (userSongs) => { // get the artists and score them
     userSongs.forEach(song => {
         if (artistScore.has(song.artist)) {
-            artistScore.set(song.artist, artistScore.get(song.artist) + 1);
+            artistScore.set(song.artist, artistScore.get(song.artist) + 2);
         } else {
             artistScore.set(song.artist, 1);
         }
     });
 };
 
+const reduceRepeats = (songs) => {
+    console.log('artist score prior ', artistScore)
+    const artistCount = new Map();
+    songs.forEach(song => {
+        if (artistCount.has(song.artist)) {
+            if (artistCount.get(song.artist) > blendLimit * .3) {
+                artistScore.set(song.artist, artistScore.get(song.artist) + 1);
+            }
+            else {
+                song[0] -= 50;
+            }
+        }
+        else {
+            artistCount.set(song.artist, 1);
+        }
+    });
+    console.log('reduce repeats artist counts', artistCount)
+};
+
+// const reduceRepeats = (songs) => {
+//     const artistCount = new Map();
+//     songs.forEach(song => {
+//         if (artistCount[song.artist]) {
+//             if (artistCount[song.artist] > blendLimit * .3) {
+//                 artistScore.set(song.artist, artistScore.get(song.artist) + 1);
+//             }
+//             else {
+//                 song[1][0] -= 50;
+//             }
+//         }
+//         else {
+//             artistCount.set(song.artist, 1);
+//         }
+//     });
+// };
+
 // does the blend using values inputted from html
 const blendPlaylist = async (username1, username2) => {
-    genreScore = new Map();
-    artistScore = new Map();
-    songPriority = new Map();
-
-    user1weight = 25;
-    user2weight = 25;
     try {
         // Get usernames from input fields
         //const username1 = document.getElementById("username1").value;
         //const username2 = document.getElementById("username2").value;
-        console.log(username1);
-        console.log(username2);
+
         // Fetch top tracks for both users
         const [user1TopTracks, user2TopTracks] = await Promise.all([getUserTopTracks(username1), getUserTopTracks(username2)]);
 
@@ -126,25 +140,39 @@ const blendPlaylist = async (username1, username2) => {
         updateArtistScores(user2TopTracks);
         updateGenreScores(user1TopTracks);
         updateGenreScores(user2TopTracks);
-        updateSongScores(user1TopTracks, username1, true);
-        updateSongScores(user2TopTracks, username2, false);
+        updateSongScores(user1TopTracks, username1);
+        updateSongScores(user2TopTracks, username2);
 
         // Sort genre score and song priority maps
         const sortedSongs = new Map([...songPriority.entries()].sort((a, b) => b[1][0] - a[1][0]));
-        // console.log(user1TopTracks);
-        // console.log(user2TopTracks);
-        // console.log(sortedSongs);
-        // console.log(songPriority);
+        console.log("sorted songs prior to reduce ", sortedSongs );
+        reduceRepeats(sortedSongs);
+        console.log("sorted songs after reduce  ", sortedSongs );
+        const sortedSongs2 = new Map([...sortedSongs.entries()].sort((a, b) => b[1][0] - a[1][0]));
+        
+        // Get the top blended songs based on song priority
+        const blendedSongs = Array.from(sortedSongs2.keys()).slice(0, blendLimit);
+
+        // Shuffle the blend to make it a lil more random
+        const shuffledSongs = blendedSongs
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value)
+
+        //console.log(user1TopTracks);
+        //console.log(user2TopTracks);
+        //console.log(sortedSongs);
+        console.log(songPriority);
         //console.log(genreScore);
         //console.log(artistScore);
 
         const sortedGenres = new Map([...genreScore.entries()].sort((a, b) => b[1] - a[1]));
-        //console.log(sortedGenres);
+        console.log(sortedGenres);
         const sortedArtists = new Map([...artistScore.entries()].sort((a, b) => b[1] - a[1]));
-        //console.log(sortedArtists);
+        console.log(sortedArtists);
+        console.log(shuffledSongs);
 
-        // Get the top blended songs based on song priority
-        const blendedSongs = Array.from(sortedSongs.keys()).slice(0, blendLimit);
+        
 
         // Update the playlist in the HTML
         /*
@@ -157,7 +185,7 @@ const blendPlaylist = async (username1, username2) => {
             playlistItems.appendChild(listItem);
         });
         */
-       return blendedSongs;
+       return shuffledSongs;
     } catch (error) {
         console.error('Error creating blend playlist:', error);
     }
