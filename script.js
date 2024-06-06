764ee334080df1a0ff920c84b9e484681483d3d3
Justin Wong <justinwong@eduroam-169-233-231-183.ucsc.edu>
2024-06-06 16:08:39 -0700

    almost got profile pics

--- script.js
+++ script.js
@@ -7,20 +7,44 @@ const genreScore = new Map();
 const artistScore = new Map();
 const songPriority = new Map();
 
+
+const getUserProfile = async (username) => {
+    const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${username}&api_key=${apiKey}&format=json`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const user = data.user;
+        if (user.image[0]['#text'] == '') {
+            return 'https://lastfm.freetls.fastly.net/i/u/avatar170s/818148bf682d429dc215c1705eb27b98.png';
+        }
+        return user.image[0]['#text'];
+        //console.log(data);
+        //console.log(data.image);
+        //console.log(typeof data.image);
+        return data.image;
+    }
+    catch (error) {
+        return error;
+    }
+}
+
 const getUserTopTracks = async (username) => {
     const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username}&period=${period}&limit=${limit}&api_key=${apiKey}&format=json`;
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
+        const link = await getUserProfile(username);
         const tracksWithGenres = await Promise.all(data.toptracks.track.map(async (track) => {
             const genre = await getTrackTopTags(track.artist.name, track.name);
             return {
+                user: username,
                 name: track.name,
                 artist: track.artist.name,
                 genre: genre,
 
                 // Added this to include the track's URL
-                uri: track.url
+                uri: track.url,
+                img: link
             };
         }));
         return tracksWithGenres;
@@ -35,7 +59,7 @@ const getTrackTopTags = async (artist, track) => {
     try {
         const response = await fetch(apiUrl2);
         const data = await response.json();
-        console.log(data.toptags);
+        ////console.log(data.toptags);
         return (data.toptags.tag ? data.toptags.tag.map(tag => tag.name) : []);
     } catch (error) {
         return [];
@@ -45,7 +69,7 @@ const getTrackTopTags = async (artist, track) => {
 const updateSongScores = (userSongs) => {
     userSongs.forEach(song => {
         song.genre = song.genre.slice(0, 10);
-        console.log("genres for", song.name, ":", song.genre);
+        //console.log("genres for", song.name, ":", song.genre);
         Array.prototype.forEach.call(song.genre, genre => {
             if (genreScore.has(genre)) {
                 genreScore.set(genre, genreScore.get(genre) + 0.1);
@@ -62,16 +86,16 @@ const updateSongScores = (userSongs) => {
         const songKey = `${song.name} - ${song.artist}`;
         const songIndex = userSongs.indexOf(song);
         if (songPriority.has(songKey)) {
-            console.log("already in:", songPriority.get(songKey), (10 * Math.log(limit - songIndex)), 50);
+            //console.log("already in:", songPriority.get(songKey), (10 * Math.log(limit - songIndex)), 50);
             songPriority.set(songKey, songPriority.get(songKey) + (10 * Math.log(limit - songIndex)) + 50);
         } else {
             if (song.genre) {
                 const genresScore = song.genre.reduce((totalScore, genre) => totalScore + genreScore.get(genre), 0);
-                console.log("genre:", Math.log(artistScore.get(song.artist)), genresScore/2, (10 * Math.log(limit - songIndex)))
+                //console.log("genre:", Math.log(artistScore.get(song.artist)), genresScore/2, (10 * Math.log(limit - songIndex)))
                 songPriority.set(songKey, Math.log(artistScore.get(song.artist)) + genresScore/2 + (10 * Math.log(limit - songIndex)) + 1);
             }
             else {
-                console.log("no genre:", Math.log(artistScore.get(song.artist)), (10 * Math.log(limit - songIndex)))
+                //console.log("no genre:", Math.log(artistScore.get(song.artist)), (10 * Math.log(limit - songIndex)))
                 songPriority.set(songKey, Math.log(artistScore.get(song.artist)) + (10 * Math.log(limit - songIndex)) + 1);
             }
         }
@@ -94,6 +118,7 @@ const blendPlaylist = async (username1, username2) => {
             const song = user1TopTracks.concat(user2TopTracks).find(s => s.name === name && s.artist === artist);
             return song;
         });
+        //console.log(blendedSongs[49].img);
        return blendedSongs;
     } catch (error) {
         console.error('Error creating blend playlist:', error);
